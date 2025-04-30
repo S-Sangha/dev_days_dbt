eab4fd762f17a7a2a6496838a6222e55443adaf6
etleap-transforms-test <etleap-transforms-test@etleap.com>
2025-04-30 15:41:17 -0700

    Failing PR
    should fail dbt build

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -4,8 +4,8 @@
 ) }}
 
 SELECT
-    id AS product_id,
-    name AS product_name,
+    id AS product_id
+    name AS product_name
     description AS product_description,
     cost AS product_cost
 FROM {{ source('shannon_test', 'products') }}
